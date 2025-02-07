cd91a3118f56a6834819515db7b432145dfdab9b
Faith Adeoti <faith.adeoti@venturegardengroup.com>
2025-02-07 13:05:52 +0100

    Clean ups

--- src/components/navigation/NavUser.tsx
+++ src/components/navigation/NavUser.tsx
@@ -72,7 +72,7 @@ export function NavUser({
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
                   <AvatarImage src={user.avatar} alt={user.name} />
-                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                  <AvatarFallback className="rounded-lg">FA</AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-15 leading-tight">
                   <span className="truncate">{user.name}</span>
@@ -83,23 +83,8 @@ export function NavUser({
             <DropdownMenuSeparator />
             <DropdownMenuGroup>
               <DropdownMenuItem>
-                <Sparkles />
-                Upgrade to Pro
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
                 <BadgeCheck />
                 Account
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <CreditCard />
-                Billing
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Bell />
-                Notifications
               </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
